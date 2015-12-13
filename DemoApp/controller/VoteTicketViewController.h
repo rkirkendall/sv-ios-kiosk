4afc036b6a6082c99ea759162e56760df59a2dfd
rickykirkendall <r1cky1337@gmail.com>
2015-12-12 19:12:36 -0500

    Moved vote ticket vc

---
+++ DemoApp/controller/VoteTicketViewController.h
@@ -0,0 +1,22 @@
+//
+//  VoteTicketViewController.h
+//  DemoApp
+//
+//  Created by Ricky Kirkendall on 12/12/15.
+//  Copyright © 2015 SureVoting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BButton.h"
+@interface VoteTicketViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *logoImageView;
+@property (weak, nonatomic) IBOutlet UILabel *remainingVotesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *chosenVotesLabel;
+@property (weak, nonatomic) IBOutlet BButton *continueButton;
+@property (weak, nonatomic) IBOutlet UILabel *votingForLabel;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+
+- (IBAction)continueTapped:(id)sender;
+
+@end
